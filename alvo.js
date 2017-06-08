3b85aab1410d5421fe2f2e991c2eb20c317ed1c9
igorSilCar <igor.embromation@gmail.com>
2017-06-08 00:51:47 -0300

    Empresas com nota para cdapv
    Uma tabela simples hospedada na internet para podermos anotar e consultar empresas que emitem nota para utilizacao de verba de pequeno vulto e manutencao na agencia. Prototipo bem inicial, provavelmente nao funciona como o esperado e precisa ser atualizado com mais funcoes, melhor controle de acesso e mais robustes. Toda ajuda eh bem vinda

---
+++ alvo.js
@@ -0,0 +1,45 @@
+function closeInput() {
+	document.getElementById('spoiler').style.display = 'none';
+}
+var app = {
+	el : document.getElementById('corpo'),
+	rows : [],
+	fetchAll : function() {
+		var data = '';
+		if (this.rows.length > 0) {
+			for (i = 0; i < this.rows.length; i++) {
+				data += '<tr>';
+				for (j = 0; j < this.rows[i].length; j++) {
+					data += '<td>' + this.rows[i][j] + '</td>';
+				}
+				data += '<td><button onclick="app.edit(' + i + ')">Alterar</button></td>';
+				data += '</tr>';
+			}
+		}
+		return this.el.innerHTML = data;
+	},
+	addrow : function () {
+		var elem = document.getElementById('add-row'); 
+		var row = elem.value.split(',', 4);
+		this.rows.push(row);
+		this.fetchAll();
+		elem.value = '';
+	},
+	edit : function (item) {
+		//alert(item);
+		var elem = document.getElementById('edit-row');
+		elem.value = this.rows[item];
+		document.getElementById('spoiler').style.display = 'block';
+		self = this;
+		document.getElementById('saveEdit').onsubmit = function() {
+			var row = elem.value;
+			if (row) {
+				self.rows.splice(item, 1, row.split(',', 4));
+				self.fetchAll();
+				closeInput();
+			}
+		}
+	}
+};
+
+app.fetchAll();
